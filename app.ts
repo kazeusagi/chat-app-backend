4ea2416a40f3836112578fab99fb44ff2a4db2f9
kazeusagi <toshiki1098@gmail.com>
2024-05-01 12:36:22 +0000

    prettier設定途中

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import OpenAI from 'openai'
+
+const openai = new OpenAI();
 
 const app = express()
 const port = 3333
@@ -12,8 +15,19 @@ app.get('/', async(req: any, res: any) => {
 })
 
 app.post('/aa', async(req: any, res: any) => {
+    req.body
     res.send("a")
 })
+
+// app.post('/chat', async(req: any, res: any) => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: "みうく飲みたい" }],
+//     model: "gpt-3.5-turbo",
+//   });
+
+//   console.log(completion.choices[0]);
+//   res.send(completion.choices[0].message.content)
+// })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
