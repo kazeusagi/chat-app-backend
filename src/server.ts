465e2d6d2f9d001a9d15ac3989a5d5b1fe794dfe
kazeusagi <toshiki1098@gmail.com>
2024-05-02 22:58:15 +0000

    DBとの連携実装途中・askAPI実装

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ const prisma = new PrismaClient();
 const app = express();
 const port = 3333;
 
+// jsonパースの設定
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // ミドルウェアの登録
 const middleware = (req: any, res: any, next: any) => {
   console.log(req.method, req.url, 'called');
@@ -22,28 +26,35 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 // ルーティング
 app.get('/', async (req: any, res: any) => {
   res.send('Welcome');
-  const user = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-    },
-  });
 });
 
 app.post('/aa', async (req: any, res: any) => {
   req.body;
-  res.send('a');
+  console.log(req.body.name);
+  res.send(req.body.name);
 });
 
-// app.post('/chat', async(req: any, res: any) => {
-//   const completion = await openai.chat.completions.create({
-//     messages: [{ role: "system", content: "みうく飲みたい" }],
-//     model: "gpt-3.5-turbo",
-//   });
+app.post('/chat', async (req: any, res: any) => {
+  const requestMessage = req.body.message;
+  if (requestMessage == undefined) return;
 
-//   console.log(completion.choices[0]);
-//   res.send(completion.choices[0].message.content)
-// })
+  // openaiにリクエスト
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: requestMessage }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion);
+
+  const responseMessage = completion.choices[0].message.content;
+  // DBに保存
+  const chat = await prisma.chat.create({});
+  const message = await prisma.message.create({
+    data: { content: responseMessage || '', chatId: chat.id },
+  });
+
+  res.send(message);
+});
 
 // サーバー起動
 app.listen(port, () => {
