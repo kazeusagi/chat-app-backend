1f4b87859ebbc83ad56425c18b8aab3009d10cb1
kazeusagi <toshiki1098@gmail.com>
2024-05-02 02:44:37 +0000

    コンパイラの設定見直し

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,17 @@ const openai = new OpenAI();
 const app = express();
 const port = 3333;
 
+// ミドルウェアの登録
+const middleware = (req: any, res: any, next: any) => {
+  console.log(req.method, req.url, 'called');
+  next();
+};
+app.use(middleware);
+
 // localhost:3000からのアクセスを許可
 app.use(cors({ origin: 'http://localhost:3000' }));
 
+// ルーティング
 app.get('/', async (req: any, res: any) => {
   res.send('Welcome');
 });
@@ -29,6 +37,7 @@ app.post('/aa', async (req: any, res: any) => {
 //   res.send(completion.choices[0].message.content)
 // })
 
+// サーバー起動
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
